b6a797ceb70399919db3289cccc5693afda4a01b
inange <37964530+inange@users.noreply.github.com>
2018-04-01 15:35:46 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -35,7 +35,7 @@ client.on("message", async message => {
   if(message.content.indexOf(config.prefix) !== 0) return;
   
 
-  const args = message.content.slice(config.prefix.length).trim().split(/ ++/g);
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
   
 

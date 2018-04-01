4013c9d624463a7b76eb51b3a113062a292d78d5
inange <37964530+inange@users.noreply.github.com>
2018-04-01 10:06:02 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,19 +12,19 @@ client.on("ready", () => {
  
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
  
-  client.user.setGame(` ${client.guilds.size} sv ye bulasti`);
+  client.user.setGame(` ${client.guilds.size} sv ye bulasti ++help`);
 });
 
 client.on("guildCreate", guild => {
   
   console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-  client.user.setGame(`++help`);
+  client.user.setGame(`${client.guilds.size} sv ye bulasti ++help`);
 });
 
 client.on("guildDelete", guild => {
  
   console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
-  client.user.setGame(`on ${client.guilds.size} servers`);
+  client.user.setGame(`${client.guilds.size} sv ye bulasti ++help`);
 });
 
 
